484293c87554d14ed27e497b2bf7e3056b733258
Simon <simon.ropiot44@gmail.com>
2026-05-15 02:05:54 +0200

    new:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         foreground: "var(--foreground)",
       },
       animation: {
-        marquee: "marquee-scroll 90s linear infinite",
+        marquee: "marquee-scroll 120s linear infinite",
       },
       keyframes: {
         "marquee-scroll": {
